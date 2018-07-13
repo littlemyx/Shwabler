c8b253553ff4a23f642d18de03740e82c3080b82
andrey muhin <a.muhin@corp.mail.ru>
2018-07-13 14:00:25 +0300

    seed based color of card & buttons with icons

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,22 @@
 <template>
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
-        <v-card v-bind:color="color" class="white--text">
+        <v-card v-bind:color="color" class="white--text darken-3">
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
               <div>{{card_text}}</div>
             </div>
           </v-card-title>
+
+          <v-card-actions>
+            <v-btn fab dark small v-bind:color="color" class="lighten-3">
+              <v-icon dark>favorite</v-icon>
+            </v-btn>
+            <v-btn fab dark small v-bind:color="color" class="darken-1">
+              <v-icon dark>clear</v-icon>
+            </v-btn>
+          </v-card-actions>
         </v-card>
     </v-flex>
   </v-layout>
@@ -15,13 +24,7 @@
 
 <script>
 import MessageCard from '../components/MessageCard.vue';
-const colors = [
-  'red',
-  'pink',
-  'purple',
-  'indigo',
-  'green'
-];
+const {colors} = require('../assets/data/colors.json');
 
 export default {
   data () {
@@ -30,10 +33,18 @@ export default {
     }
   },
   computed: {
-    color: function () {
+    color: (function () {
       const length = colors.length;
-      return colors[Math.round(-0.5 + Math.random() * (length + 1))] + ' darken-3';
-    }
+      return function () {
+        let seed = this.card_text.slice(0, 5);
+        seed = seed.split('').map((x) => {
+          return x.charCodeAt(0);
+        });
+        seed = seed.reduce((x, y) => { return x + y });
+        seed %= length;
+        return colors[seed];
+      }
+    }())
   },
   components: {
     MessageCard
