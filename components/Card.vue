c4bdf60c8a1badf34f006bd1ac0b0df34bfaaa22
andrey muhin <a.muhin@corp.mail.ru>
2018-07-13 14:54:37 +0300

    add props to Card component

--- components/Card.vue
+++ components/Card.vue
@@ -4,17 +4,18 @@
         <v-card v-bind:color="color" class="white--text darken-3">
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-              <div>{{card_text}}</div>
+              <h3 class="headline mb-0">{{ this.title }}</h3>
+              <div>{{ this.cardText }}</div>
             </div>
           </v-card-title>
 
           <v-card-actions>
-            <v-btn fab dark small v-bind:color="color" class="lighten-3">
-              <v-icon dark>favorite</v-icon>
+            <v-btn fab dark v-bind:color="color" class="lighten-3 ml-3">
+              <v-icon medium dark>favorite</v-icon>
             </v-btn>
-            <v-btn fab dark small v-bind:color="color" class="darken-1">
-              <v-icon dark>clear</v-icon>
+            <v-spacer></v-spacer>
+            <v-btn fab dark v-bind:color="color" class="darken-1 mr-3">
+              <v-icon medium dark>clear</v-icon>
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -27,20 +28,31 @@ import MessageCard from '../components/MessageCard.vue';
 const {colors} = require('../assets/data/colors.json');
 
 export default {
-  data () {
-    return {
-      card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui.'
+  props: {
+    title: {
+      type: String,
+      required: true,
+      validator: function (value) {
+        return true;
+      }
+    },
+    cardText: {
+      type: String,
+      required: true,
+      validator: function (value) {
+        return true;
+      }
     }
   },
   computed: {
     color: (function () {
       const length = colors.length;
       return function () {
-        let seed = this.card_text.slice(0, 5);
+        let seed = this.cardText.slice(0, 5);
         seed = seed.split('').map((x) => {
           return x.charCodeAt(0);
         });
-        seed = seed.reduce((x, y) => { return x + y });
+        seed = seed.reduce((x, y) => { return x + y }, 6);
         seed %= length;
         return colors[seed];
       }
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
-        <Card></Card>
-        <button @click="goBack">
+        <Card v-bind:title="title" v-bind:cardText="cardText"></Card>
+        <v-btn depressed color="error" @click="goBack">
           Back
-        </button>
+        </v-btn>
     </div>
 </template>
 
 <script>
 import Card from '../components/Card.vue';
 export default {
+  data () {
+    return {
+      title: 'Мама мыла раму',
+      cardText: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui.'
+    }
+  },
   methods: {
     goBack () {
       window.history.length > 1
