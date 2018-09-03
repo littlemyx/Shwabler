5d2d55961135cd6cf200d284796497cfd5687a54
andrey muhin <a.muhin@corp.mail.ru>
2018-09-03 23:01:48 +0300

    add message component

---
+++ components/DialogBody.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <Message
+      :key="i"
+      class="message"
+      v-for="(message, i) in messages"
+      v-bind:author="message.author" 
+      v-bind:text="message.text"
+    />
+  </v-layout>
+</template>
+
+<script>
+import Message from './Message.vue';
+
+export default {
+  props: {
+    messages: {
+      type: Array,
+      required: false
+    }
+  },
+  components: {
+    Message
+  }
+}
+</script>
---
+++ components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{this.text}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
