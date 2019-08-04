a182ec0c44bd33d7ff0590c016552607f795b8d3
andrey muhin <a.muhin@corp.mail.ru>
2019-08-04 22:07:12 +0300

    scrolling pages

---
+++ components/Indroduction.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout align-center justify-center column fill-height>
+    <div class="logoWrapper">
+      <p>{{ $t("selling_text") }}</p>
+    </div>
+    
+  </v-layout >
+</template>
+
+<script>
+import RouterLink from "../components/RouterLink"
+export default {
+  components: {
+    RouterLink
+  },
+  middleware: "index",
+  layout: "centerLayout"
+}
+</script>
+
+<style scoped>
+.logoWrapper {
+  position: relative;
+}
+
+.beta {
+  position: absolute;
+  top: 0;
+  right: -26px;
+}
+</style>
+>
--- layouts/centerLayout.vue
+++ layouts/centerLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="centered">
     <v-content>
-      <v-container fluid fill-height>
+      <v-container fluid fill-height pa-0>
         <router-view/>
       </v-container>
     </v-content>
