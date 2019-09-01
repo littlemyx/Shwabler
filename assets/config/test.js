053d372d57eb134cf5122b4e454718b3095c796a
a.muhin <a.muhin@a.muhin>
2019-09-01 23:35:16 +0300

    test base

---
+++ assets/config/test.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyB8YN2h-Ei_S0WRH3Eer1fhIt32MBsioko",
+  authDomain: "shwablertest.firebaseapp.com",
+  databaseURL: "https://shwablertest.firebaseio.com",
+  projectId: "shwablertest",
+  storageBucket: "shwablertest.appspot.com",
+  messagingSenderId: "811857236248",
+  appId: "1:811857236248:web:ea61eed56bef1d1e"
+}
